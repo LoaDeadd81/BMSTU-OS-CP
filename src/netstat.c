4761d9a0ffe35f07d8365721e562b00c44343d30
LoaDeadd <vvolkv81494@gmail.com>
2024-02-06 23:02:55 +0300

    without idef0

--- src/netstat.c
+++ src/netstat.c
@@ -13,94 +13,32 @@ MODULE_AUTHOR("George Volkov");
 
 #define FILENAME "netstat"
 
-void print_qdisc_data(struct seq_file *seq, struct Qdisc *q){
-	if(q == NULL) return;
-
-	struct gnet_stats_queue *stats;
-
-	seq_printf(seq, "\tQdisc: %d %d\n", q->flags, q->limit);
-
-	for (int i = 0; i < nr_cpu_ids; i++)
-	{
-		if (cpu_online(i)) {
-			stats = per_cpu_ptr(q->cpu_qstats, i);
-			if(stats == NULL) continue;
-			seq_printf(seq, "\tQdisc cpu#%d: %d %d %d %d\n", i, stats->qlen, stats->backlog, stats->drops, stats->overlimits);
-		}
-	}
-}
-
-int get_cq_len(struct sk_buff *cq) {
-	if(cq == NULL) return 0;
-	
-	int cnt = 0;
-
-	while (cq->next != NULL) {
-		cnt++;
-		cq = cq->next;
-	}
-
-	return cnt;
-}
-
-void print_softnet_data(struct seq_file *seq, struct softnet_data *sd){
-	seq_printf(seq, "softnet_data cpu #%d: %d %d %d %d %d %d\n", 
-	sd->cpu, sd->processed, sd->time_squeeze, sd->received_rps, sd->dropped, get_cq_len(sd->completion_queue), sd->in_net_rx_action);
-	print_qdisc_data(seq, sd->output_queue);
-}
-
-void print_dev_data(struct seq_file *seq, struct net_device *dev){
-	if (dev == NULL) return;
-	seq_printf(seq, "\tnetdev: %s %ld %d\n", dev->name, dev->state, dev->mtu);
-
-	// if(dev == NULL || dev->netdev_ops == NULL || dev->netdev_ops->ndo_get_stats == NULL) return;
-	// struct net_device_stats* stats = dev->netdev_ops->ndo_get_stats(dev);
-	// seq_printf(seq, "\tnetdev rx: %ld %ld %ld %ld\n", stats->rx_packets, stats->rx_bytes, stats->rx_errors, stats->rx_dropped);
-	// seq_printf(seq, "\tnetdev tx: %ld %ld %ld %ld\n", stats->tx_packets, stats->tx_bytes, stats->tx_errors, stats->tx_dropped);
-}
-
-void print_napi_data(struct seq_file *seq, struct napi_struct *n){
+void print_backlog_napi_data(struct seq_file *seq, struct napi_struct *n){
 	if(n == NULL) return;
 	
-	seq_printf(seq, "\tnapi: %ld %d %d %d\n", n->state, n->weight, n->list_owner, n->rx_count);
-	print_dev_data(seq, n->dev);
-}
-
-void print_poll_list_data(struct seq_file *seq, struct softnet_data *sd){
-	long i = 0;
-	for(i = 0; i < INT_MAX; i++) {
-		if(list_empty(&sd->poll_list)){
-			
-		}
-		else{
-			LIST_HEAD(list);
-			list_splice_init(&sd->poll_list, &list);
-			if(list_empty(&list)) break;
-
-			struct list_head *head = &list;
-			struct list_head *cur = &list;
-			struct napi_struct *n;
-
-			list_for_each(cur, head){
-				n = list_entry(cur, struct napi_struct, poll_list);
-				print_napi_data(seq, n);
-			}
-			return;
-		} 
-	}
-
-	seq_printf(seq, "\tpoll_list is empty %ld\n", i);
+	seq_printf(seq, "\tbacklog_napi: %ld %d %d %d %d\n", n->state, n->weight, n->list_owner, n->rx_count, n->napi_id);
+	seq_printf(seq, "\tgro_buckets: ");
+	for(int i = 0; i < GRO_HASH_BUCKETS; i++) seq_printf(seq, "%d ", n->gro_hash->count);
+	seq_printf(seq, "\n");
 }
 
 void print_backlog_data(struct seq_file *seq, struct softnet_data *sd) {
-	seq_printf(seq, "\tbacklog_q_len: %d\n", skb_queue_len(&sd->input_pkt_queue));
-	print_napi_data(seq, &sd->backlog);
+	seq_printf(seq, "\tbacklog: %d %d %d %d\n", sd->received_rps, skb_queue_len(&sd->input_pkt_queue), skb_queue_len(&sd->process_queue), sd->input_queue_tail);
+}
+
+void print_softnet_data(struct seq_file *seq, struct softnet_data *sd){
+	seq_printf(seq, "softnet_data cpu #%d: %d %d %d %d\n", sd->cpu, sd->processed, sd->time_squeeze, sd->dropped, sd->in_net_rx_action);
 }
 
 void print_netdata(struct seq_file *seq, struct softnet_data *sd) {
-	print_softnet_data(seq, sd);
-	print_poll_list_data(seq, sd);
-	print_backlog_data(seq, sd);
+	for(int i = 0; i < INT_MAX; i++) {
+		if(!list_empty(&sd->poll_list)){
+			print_softnet_data(seq, sd);
+			print_backlog_data(seq, sd);
+			print_backlog_napi_data(seq, &sd->backlog);
+			break;
+		}
+	}
 }
 
 int netstat_show(struct seq_file *seq, void *v)
